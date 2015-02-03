297558c65f67c3c68c5a35cea3ce59c5c53f5e1d
Sven Bohm <bohms@msu.edu>
2015-02-03 18:33:49 -0500

    basic campbell time calc

--- weather.go
+++ weather.go
@@ -2,14 +2,15 @@ package main
 
 /* import "github.com/gin-gonic/gin" */
 import "time"
-import "fmt"
 
 func CampbellTime(myTime time.Time) [3]int {
- fmt.Println("here") 
-  hourmin := ""
-  return [3]int{myTime.Year(),myTime.YearDay(),hourmin}
+	hourmin := myTime.Hour()*100 + myTime.Minute()
+	if hourmin == 0 {
+		hourmin = 2400
+	}
+	return [3]int{myTime.Year(), myTime.YearDay(), hourmin}
 }
 
 func main() {
-  /* router := gin.Default() */
+	/* router := gin.Default() */
 }
--- weather_test.go
+++ weather_test.go
@@ -2,33 +2,32 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
+	"log"
 	"testing"
 	"time"
-  "log"
 )
 
 var timeTest = []struct {
-  in string
-  out [3]int
+	in  string
+	out [3]int
 }{
-{
-  in: "2014-01-30T10:30:06+05:00",
-  out: [3]int{2014,30,1030},
-},
-{
-  in: "2013-02-03T00:00:00+05:00",
-  out: [3]int{2013,34,2400},
-},
+	{
+		in:  "2014-01-30T10:30:06+05:00",
+		out: [3]int{2014, 30, 1030},
+	},
+	{
+		in:  "2013-02-03T00:00:00+05:00",
+		out: [3]int{2013, 34, 2400},
+	},
 }
 
 func TestCambpellTime(t *testing.T) {
-  log.Info("testing")
-  for _, test := range timeTest {
-    mytime, err := time.Parse(time.RFC3339, test.in)
-    if err != nil {
-      log.Fatal(err)
-    }
-    actual := CampbellTime(mytime)
-    assert.Equal(t, test.out, actual)
-  }
+	for _, test := range timeTest {
+		mytime, err := time.Parse(time.RFC3339, test.in)
+		if err != nil {
+			log.Fatal(err)
+		}
+		actual := CampbellTime(mytime)
+		assert.Equal(t, test.out, actual)
+	}
 }
