1c368d9ea21a973e21d4e6385f398ab84cee2e36
Sven Bohm <bohms@msu.edu>
2015-02-26 15:42:15 -0500

    started on daily mawn weather

--- weather-tap.go
+++ weather-tap.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
@@ -87,7 +88,92 @@ func tablesById(db *sqlx.DB, c *gin.Context) {
 	c.JSON(200, results)
 }
 
+type DayObservationDB struct {
+	Battery_voltage_min          sql.NullFloat64
+	Year_rtm                     sql.NullInt64
+	Day_rtm                      sql.NullInt64
+	Hour_rtm                     sql.NullInt64
+	Air_temp_107_max             sql.NullFloat64
+	Air_temp_107_hour_max        sql.NullInt64
+	Air_temp_107_min             sql.NullFloat64
+	Air_temp_107_hour_min        sql.NullInt64
+	Rh_max                       sql.NullFloat64 // rh_max * 100  float
+	Rh_hour_max                  sql.NullInt64
+	Rh_min                       sql.NullFloat64 //rh_min * 100 float
+	Rh_hour_min                  sql.NullInt64
+	Sol_rad_max                  sql.NullFloat64 //sol_rad_max * (0.6977 * 60)
+	Sol_rad_hour_max             sql.NullInt64
+	Soil_temp_5_cm_max           sql.NullFloat64
+	Soil_temp_5_cm_hour_max      sql.NullInt64
+	Soil_temp_5_cm_min           sql.NullFloat64
+	Soil_temp_5_cm_hour_min      sql.NullInt64
+	Soil_temp_10_cm_max          sql.NullFloat64
+	Soil_temp_10_cm_hour_max     sql.NullInt64
+	Soil_temp_10_cm_min          sql.NullFloat64
+	Soil_temp_10_cm_hour_min     sql.NullInt64
+	Soil_moisture_10_cm_max      sql.NullFloat64
+	Soil_moisture_10_cm_hour_max sql.NullInt64
+	Soil_moisture_10_cm_min      sql.NullFloat64
+	Soil_moisture_10_cm_hour_min sql.NullInt64
+	Soil_moisture_25_cm_max      sql.NullFloat64
+	Soil_moisture_25_cm_hour_max sql.NullInt64
+	Soil_moisture_25_cm_min      sql.NullFloat64
+	Soil_moisture_25_cm_hour_min sql.NullInt64
+	Wind_speed_max               sql.NullFloat64
+	Wind_speed_hour_max          sql.NullInt64
+	Rain_mm                      sql.NullFloat64
+	Date                         time.Time
+	Solar_radiation              sql.NullFloat64
+	Precip                       sql.NullFloat64
+}
+
 func day_observations(db *sqlx.DB, c *gin.Context) {
+	rows, err := db.Queryx("select * from weather.day_observations_cache")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		observation := DayObservationDB{}
+		if err := rows.StructScan(&observation); err != nil {
+			log.Fatal(err)
+		}
+
+		observation.Sol_rad_max.Float64 = observation.Sol_rad_max.Float64 * (0.6977 * 60)
+		observation.Rh_max.Float64 = observation.Rh_max.Float64 * 100
+		observation.Rh_min.Float64 = observation.Rh_min.Float64 * 100
+		data, _ := json.Marshal(observation)
+		c.Writer.Write(data)
+
+	}
+	/* solar_radiation = nil */
+	/* if solar_radiation */
+	/*   solar_radiation = self.solar_radiation * 86.4 */
+	/* end */
+
+	/* sprintf("24,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s, %s,%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s,%s,%s,%s, %s,%s, %s, %s, %s", */
+	/*         year_rtm, day_rtm,hour_rtm, */
+	/*         self.air_temp_107_max, */
+	/*         self.air_temp_107_hour_max,  self.air_temp_107_min, */
+	/*         self.air_temp_107_hour_min, */
+	/*         self.rh_max * 100, self.rh_hour_max, self.rh_min * 100, */
+	/*         self.rh_hour_min, solar_radiation, */
+	/*         self.sol_rad_max * (0.6977 * 60) , self.sol_rad_hour_max, */
+	/*         self.soil_temp_5_cm_max,soil_temp_5_cm_hour_max, */
+	/*         soil_temp_5_cm_min, soil_temp_5_cm_hour_min, */
+	/*         self.soil_temp_10_cm_max, */
+	/*         soil_temp_10_cm_hour_max, */
+	/*         self.soil_temp_10_cm_min, */
+	/*         soil_temp_10_cm_hour_min, */
+	/*         self.soil_moisture_10_cm_max, self.soil_moisture_10_cm_hour_max, */
+	/*         self.soil_moisture_10_cm_min, */
+	/*         self.soil_moisture_10_cm_hour_min, self.soil_moisture_25_cm_max, */
+	/*         self.soil_moisture_25_cm_hour_max, */
+	/*         self.soil_moisture_25_cm_min, self.soil_moisture_25_cm_hour_min, */
+	/*         self.wind_speed_max, */
+	/*         self.wind_speed_hour_max, self.rain_mm, self.battery_voltage_min, */
+	/*         self.date) */
 }
 
 func hour_observations(db *sqlx.DB, c *gin.Context) {
@@ -109,17 +195,15 @@ func Router(db *sqlx.DB) *gin.Engine {
 	router.GET("/variates/:id", func(c *gin.Context) {
 		variatesById(db, c)
 	})
-	router.GET("/day_observations/:id", func(c *gin.Context) {
+	router.GET("/day_observations", func(c *gin.Context) {
 		day_observations(db, c)
 	})
 	router.GET("/hour_observations", func(c *gin.Context) {
 		hour_observations(db, c)
 	})
-	router.GET("/hour_observations/:id", Index)
 	router.GET("/five_minute_observations", func(c *gin.Context) {
 		five_minute_observations(db, c)
 	})
-	router.GET("/five_minute_observations/:id", Index)
 
 	return router
 }
