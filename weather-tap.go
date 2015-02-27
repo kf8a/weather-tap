b421273dbe0402db54d27a35530254bf5f694f2b
Sven Bohm <bohms@msu.edu>
2015-02-26 21:15:55 -0500

    finished the daily observation

--- weather-tap.go
+++ weather-tap.go
@@ -90,6 +90,14 @@ func tablesById(db *sqlx.DB, c *gin.Context) {
 	c.JSON(200, results)
 }
 
+func floatToString(value sql.NullFloat64) string {
+	return strconv.FormatFloat(value.Float64, 'f', 5, 64)
+}
+
+func intToString(value sql.NullInt64) string {
+	return strconv.FormatInt(value.Int64, 10)
+}
+
 type DayObservation struct {
 	Year_rtm                     sql.NullInt64
 	Day_rtm                      sql.NullInt64
@@ -131,42 +139,84 @@ type DayObservation struct {
 
 func (d *DayObservation) toMawn() []string {
 	value := []string{
-		strconv.FormatInt(d.Year_rtm.Int64, 10),
-		strconv.FormatInt(d.Day_rtm.Int64, 10),
-		strconv.FormatInt(d.Hour_rtm.Int64, 10),
-		strconv.FormatFloat(d.Air_temp_107_max.Float64, 'f', 5, 64),
-		// Air_temp_107_hour_max        sql.NullInt64
-		// Air_temp_107_min             sql.NullFloat64
-		// Air_temp_107_hour_min        sql.NullInt64
-		// Rh_max                       sql.NullFloat64
-		// Rh_hour_max                  sql.NullInt64
-		// Rh_min                       sql.NullFloat64
-		// Rh_hour_min                  sql.NullInt64
-		// Solar_radiation              sql.NullFloat64
-		// Sol_rad_max                  sql.NullFloat64
-		// Sol_rad_hour_max             sql.NullInt64
-		// Soil_temp_5_cm_max           sql.NullFloat64
-		// Soil_temp_5_cm_hour_max      sql.NullInt64
-		// Soil_temp_5_cm_min           sql.NullFloat64
-		// Soil_temp_5_cm_hour_min      sql.NullInt64
-		// Soil_temp_10_cm_max          sql.NullFloat64
-		// Soil_temp_10_cm_hour_max     sql.NullInt64
-		// Soil_temp_10_cm_min          sql.NullFloat64
-		// Soil_temp_10_cm_hour_min     sql.NullInt64
-		// Soil_moisture_10_cm_max      sql.NullFloat64
-		// Soil_moisture_10_cm_hour_max sql.NullInt64
-		// Soil_moisture_10_cm_min      sql.NullFloat64
-		// Soil_moisture_10_cm_hour_min sql.NullInt64
-		// Soil_moisture_25_cm_max      sql.NullFloat64
-		// Soil_moisture_25_cm_hour_max sql.NullInt64
-		// Soil_moisture_25_cm_min      sql.NullFloat64
-		// Soil_moisture_25_cm_hour_min sql.NullInt64
-		// Wind_speed_max               sql.NullFloat64
-		// Wind_speed_hour_max          sql.NullInt64
-		// Rain_mm                      sql.NullFloat64
-		// Battery_voltage_min          sql.NullFloat64
-		// Date                         time.Time
-		// Precip                       sql.NullFloat64
+		"24",
+		intToString(d.Year_rtm),
+		intToString(d.Day_rtm),
+		intToString(d.Hour_rtm),
+		floatToString(d.Air_temp_107_max),
+		intToString(d.Air_temp_107_hour_max),
+		floatToString(d.Air_temp_107_min),
+		intToString(d.Air_temp_107_hour_min),
+		floatToString(d.Rh_max),
+		intToString(d.Rh_hour_max),
+		floatToString(d.Rh_min),
+		intToString(d.Rh_hour_min),
+		floatToString(d.Solar_radiation),
+		floatToString(d.Sol_rad_max),
+		intToString(d.Sol_rad_hour_max),
+		floatToString(d.Soil_temp_5_cm_max),
+		intToString(d.Soil_temp_5_cm_hour_max),
+		floatToString(d.Soil_temp_5_cm_min),
+		intToString(d.Soil_temp_5_cm_hour_min),
+		floatToString(d.Soil_temp_10_cm_max),
+		intToString(d.Soil_temp_10_cm_hour_max),
+		floatToString(d.Soil_temp_10_cm_min),
+		intToString(d.Soil_temp_10_cm_hour_min),
+		floatToString(d.Soil_moisture_10_cm_max),
+		intToString(d.Soil_moisture_10_cm_hour_max),
+		floatToString(d.Soil_moisture_10_cm_min),
+		intToString(d.Soil_moisture_10_cm_hour_min),
+		floatToString(d.Soil_moisture_25_cm_max),
+		intToString(d.Soil_moisture_25_cm_hour_max),
+		floatToString(d.Soil_moisture_25_cm_min),
+		intToString(d.Soil_moisture_25_cm_hour_min),
+		floatToString(d.Wind_speed_max),
+		intToString(d.Wind_speed_hour_max),
+		floatToString(d.Rain_mm),
+		floatToString(d.Battery_voltage_min),
+		d.Date.String(),
+	}
+	return value
+}
+
+func (d *DayObservation) mawnHeader() []string {
+	value := []string{
+		"#code",
+		"year",
+		"day",
+		"report time",
+		"air temperature maximum",
+		"time of air temperature maximum",
+		"air temperature minimum",
+		"time of air temperature minimum",
+		"relative humidity maximum",
+		"time of relative humidity maximum",
+		"relative humidity minimum",
+		"time of relative humidity minimum",
+		"solar radiation",
+		"solar radiation density maximum",
+		"time  of solar radiation desnsity",
+		"soil temperature maximum at 5 cm under bare soil",
+		"time  of soil temperature maximum at 5 cm under bare soil",
+		"soil temperature minimum at 5 cm under bare soil",
+		"time of soil temperature minimum at 5 cm under bare soil",
+		"soil temperature maximum at 10 cm under bare soil",
+		"time of soil temperature at 10 cm  under bare soil",
+		"soil temperature minimum at 10 cm under bare soil",
+		"time of soil temperature minimum at 10 cm under bare soil",
+		"soil moisture maximum at 5 cm under sod",
+		"time of soil moisture maximum at 5 cm under sod",
+		"soil moisture minimum at 5 cm under sod",
+		"time of soil moisture minimum at 5 cm under sod",
+		"soil moisture maximum at 20 cm under sod",
+		"time of soil moisture maximum at 20 cm under sod",
+		"soil moisture minimum at  20 cm under sod",
+		"time of soil moisture minimum at 20 cm under sod",
+		"maximum wind speed",
+		"time of maximum wind speed",
+		"total precipitation",
+		"minimum data logger battery voltage",
+		"timestamp",
 	}
 	return value
 }
@@ -178,54 +228,44 @@ func day_observations(db *sqlx.DB, c *gin.Context) {
 	}
 	defer rows.Close()
 
+	i := 0
+	writer := csv.NewWriter(c.Writer)
+
+	observation := DayObservation{}
+	writer.Write(observation.mawnHeader())
 	for rows.Next() {
-		observation := DayObservation{}
 		if err := rows.StructScan(&observation); err != nil {
 			log.Fatal(err)
 		}
 
+		observation.Solar_radiation.Float64 = observation.Solar_radiation.Float64 * 86.4
 		observation.Sol_rad_max.Float64 = observation.Sol_rad_max.Float64 * (0.6977 * 60)
 		observation.Rh_max.Float64 = observation.Rh_max.Float64 * 100
 		observation.Rh_min.Float64 = observation.Rh_min.Float64 * 100
-		writer := csv.NewWriter(c.Writer)
+
 		writer.Write(observation.toMawn())
-		writer.Flush()
+
+		if i%500 == 0 {
+			writer.Flush()
+		}
+		i = i + 1
 
 	}
-	/* solar_radiation = nil */
-	/* if solar_radiation */
-	/*   solar_radiation = self.solar_radiation * 86.4 */
-	/* end */
-
-	/* sprintf("24,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s, %s,%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s, %s,%s,%s,%s, %s,%s, %s, %s, %s", */
-	/*         year_rtm, day_rtm,hour_rtm, */
-	/*         self.air_temp_107_max, */
-	/*         self.air_temp_107_hour_max,  self.air_temp_107_min, */
-	/*         self.air_temp_107_hour_min, */
-	/*         self.rh_max * 100, self.rh_hour_max, self.rh_min * 100, */
-	/*         self.rh_hour_min, solar_radiation, */
-	/*         self.sol_rad_max * (0.6977 * 60) , self.sol_rad_hour_max, */
-	/*         self.soil_temp_5_cm_max,soil_temp_5_cm_hour_max, */
-	/*         soil_temp_5_cm_min, soil_temp_5_cm_hour_min, */
-	/*         self.soil_temp_10_cm_max, */
-	/*         soil_temp_10_cm_hour_max, */
-	/*         self.soil_temp_10_cm_min, */
-	/*         soil_temp_10_cm_hour_min, */
-	/*         self.soil_moisture_10_cm_max, self.soil_moisture_10_cm_hour_max, */
-	/*         self.soil_moisture_10_cm_min, */
-	/*         self.soil_moisture_10_cm_hour_min, self.soil_moisture_25_cm_max, */
-	/*         self.soil_moisture_25_cm_hour_max, */
-	/*         self.soil_moisture_25_cm_min, self.soil_moisture_25_cm_hour_min, */
-	/*         self.wind_speed_max, */
-	/*         self.wind_speed_hour_max, self.rain_mm, self.battery_voltage_min, */
-	/*         self.date) */
+	writer.Flush()
 }
 
 func hour_observations(db *sqlx.DB, c *gin.Context) {
+
 }
 
 func five_minute_observations(db *sqlx.DB, c *gin.Context) {
+	rows, err := db.Queryx("select * from weather.lter_five_minute_a")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
 }
+
 func Router(db *sqlx.DB) *gin.Engine {
 	router := gin.Default()
 	router.GET("/tables", func(c *gin.Context) {
