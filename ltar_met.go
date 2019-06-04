db128a61bf501e04ced414ab822ac5fb187bc04d
Sven Bohm <bohms@msu.edu>
2019-06-03 22:29:34 -0400

    added md5 endpoint for ltar

--- ltar_met.go
+++ ltar_met.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "crypto/md5"
 	"database/sql"
 	"encoding/csv"
 	"github.com/gin-gonic/gin"
@@ -8,6 +9,8 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"time"
+  "fmt"
+  "strings"
 )
 
 type LtarMetObservation struct {
@@ -101,4 +104,41 @@ func ltar_met_observations(db *sqlx.DB, c *gin.Context) {
 
 	}
 	writer.Flush()
+}
+
+func ltar_met_md5(db *sqlx.DB, c *gin.Context) {
+	rows, err := db.Queryx(" select * from ( select Air_temp107_avg, Relative_humidity_avg ,Wind_speed_wvt, Wind_direction_d1_wvt, raingauge_hourly.rain_mm,Datetime, Battery_voltage_min as BatteryVoltage from weather.lter_hour_d join weather.raingauge_hourly on raingauge_hourly.hours = lter_hour_d.datetime where datetime < now() - interval '1 hour' order by datetime desc limit $1) t1 order by datetime", limit(c, 1154))
+
+	if err != nil {
+		log.Print("error in query")
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	i := 0
+  var data strings.Builder
+	writer := csv.NewWriter(&data)
+
+	obs := LtarMetObservation{}
+	writer.Write(obs.header())
+	for rows.Next() {
+		if err := rows.StructScan(&obs); err != nil {
+			log.Fatal(err)
+		}
+
+		obs.Relative_humidity_avg.Float64 = obs.Relative_humidity_avg.Float64 * 100
+
+		writer.Write(obs.to_csv())
+
+		if i%500 == 0 {
+			writer.Flush()
+		}
+		i = i + 1
+
+	}
+	writer.Flush()
+  md5value := md5.Sum([]byte(data.String()))
+  md5_slice := fmt.Sprintf("%x\n", md5value[:])
+
+  c.Writer.Write([]byte(md5_slice))
 }
--- main.go
+++ main.go
@@ -213,6 +213,9 @@ func Router(db *sqlx.DB) *gin.Engine {
 	router.GET("/weather/ltar_met.csv", func(c *gin.Context) {
 		ltar_met_observations(db, c)
 	})
+	router.GET("/weather/ltar_met.md5", func(c *gin.Context) {
+		ltar_met_md5(db, c)
+	})
 
 	return router
 }
